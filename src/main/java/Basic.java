d0d3b49f071b2f3f442cba739c33e64ca3ceae3d
olga <olga-24_@mail.ru>
2023-11-06 23:30:53 +0300

    Ответ на задание номер 4 и 6

--- src/main/java/Basic.java
+++ src/main/java/Basic.java
@@ -1,27 +1,38 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Basic {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        double i = 1;
+        double j = 1;
+        int op = 1;
 
-        System.out.println("Введите первое дробное с точкой - разделителем дробной части ");
-        double i = scanner.nextDouble();
+        try {
+            System.out.println("Введите первое дробное число с точкой - разделителем дробной части ");
+            i = scanner.nextDouble();
 
-        System.out.println("Введите второе дробное число с точкой - разделителем дробной части ");
-        double j = scanner.nextDouble();
+            System.out.println("Введите второе дробное число с точкой - разделителем дробной части ");
+            j = scanner.nextDouble();
 
-        System.out.println("Введите операцию калькулятора:\n 1 - умножение,\n 2 - сложение,\n" +
-                " 3 - вычитание,\n 4 - деление");
-        int op = scanner.nextInt();
+            System.out.println("Введите операцию калькулятора:\n 1 - умножение,\n 2 - сложение,\n" +
+                    " 3 - вычитание,\n 4 - деление");
+            op = scanner.nextInt();
 
-        if ((op != 1) && (op != 2) && (op != 3) && (op != 4)) {
-            System.out.println("Вы ввели некорректное значение операции");
-        } else {
-            System.out.println("Результат операции ");
-            Calculator calculator = new Calculator(i, j, op);
-            System.out.println(calculator.makeCalculation());
+            if ((op != 1) && (op != 2) && (op != 3) && (op != 4)) {
+                System.out.println("Вы ввели некорректное значение операции");
+            } else {
+                System.out.println("Результат операции ");
+                Calculator calculator = new Calculator(i, j, op);
+                System.out.printf("%.4f", calculator.makeCalculation());
+            }
+
+        } catch (InputMismatchException e) {
+            System.out.println("Произошла ошибка при обработке операнда или оператора: " + e);
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Результат операции отсуствует, т.к. на ноль делить нельзя");
         }
         scanner.close();
     }
-
 }
--- src/main/java/Division.java
+++ src/main/java/Division.java
@@ -1,5 +1,8 @@
 public class Division implements Operation {
-    public double calculateResult(double first, double second) {
+    public double calculateResult(double first, double second) throws ArithmeticException {
+        if (second == 0) {
+            throw new ArithmeticException();
+        }
         return first / second;
     }
 }
